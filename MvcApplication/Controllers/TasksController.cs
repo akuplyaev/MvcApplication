0331be96d07951280f889adb5ddb668d1f5a8a41
Aleksey Kuplyaev <a.kuplyaev@gmail.com>
2016-06-16 09:17:28 +0300

    Validation

--- MvcApplication/Controllers/TasksController.cs
+++ MvcApplication/Controllers/TasksController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace MvcApplication.Controllers
@@ -12,8 +13,20 @@ namespace MvcApplication.Controllers
         // GET: Tasks
         public ActionResult Index()
         {
-            Task task = new Task(1, "test1", new DateTime(2016, 07, 12));           
+            Task task = new Task(1, "test1", new DateTime(2016, 07, 12));
             return View(task);
+        }
+        [System.Web.Mvc.HttpPost]
+        public int Create([FromBody]Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
