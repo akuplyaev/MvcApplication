1bf95fb9901bf472cca604aaafb097a23b909010
Aleksey Kuplyaev <a.kuplyaev@gmail.com>
2016-06-16 06:19:23 +0300

    Create TasksController

---
+++ MvcApplication/Controllers/TasksController.cs
@@ -0,0 +1,19 @@
+﻿using MvcApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class TasksController : Controller
+    {
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            Task task = new Task(1, "test1", new DateTime(2016, 07, 12));
+            return View(task);           
+        }
+    }
+}
